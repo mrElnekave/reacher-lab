afed044d48563bfcabd09469e71c1309d4506d0a
Yamm Elnekave <klavlavigugu@gmail.com>
2023-04-28 15:04:58 -0700

    forward kinematics works

--- reacher/reacher_kinematics.py
+++ reacher/reacher_kinematics.py
@@ -17,13 +17,37 @@ def calculate_forward_kinematics_robot(joint_angles):
 
     Args:
       joint_angles: numpy array of 3 elements [TODO names]. Numpy array of 3 elements.
+    Notes:
+      joint_angles[1] is capped [-1/4pi, pi]
     Returns:
       xyz coordinates of the end-effector in the arm frame. Numpy array of 3 elements.
     """
-    # TODO for students: Implement this function. ~25-35 lines of code.
-    end_effector_xyz = np.array([0.0, 0.0, 0.0])
+    def generate_y_rotation(theta):
+        return np.array([
+            [np.cos(theta), 0, np.sin(theta)],
+            [0, 1, 0],
+            [-np.sin(theta), 0, np.cos(theta)]
+        ])
 
-    return end_effector_xyz
+    def generate_z_rotation(theta):
+        return np.array([
+            [np.cos(theta), -np.sin(theta), 0],
+            [np.sin(theta), np.cos(theta), 0],
+            [0, 0, 1]
+        ])
+
+    if joint_angles[1] < 1/4 * np.pi:
+        joint_angles[1] = 1/4 * np.pi
+
+    joint_angles *= -1
+    r1 = np.array([0, 0, L2])
+    r2 = np.array([0, 0, L1]).transpose() + \
+        np.dot(generate_y_rotation(joint_angles[2]), r1)
+    r3 = np.array([0, -HIP_OFFSET, 0]).transpose() + \
+        np.dot(generate_y_rotation(joint_angles[1]), r2)
+    r4 = np.dot(generate_z_rotation(joint_angles[0]), r3)
+
+    return r4
 
 
 def ik_cost(end_effector_pos, guess):
@@ -40,6 +64,10 @@ def ik_cost(end_effector_pos, guess):
     """
     # TODO for students: Implement this function. ~1-5 lines of code.
     cost = 0.0
+
+    cost = .5 * np.linalg.norm(
+        calculate_forward_kinematics_robot(guess) - end_effector_pos) ** 2
+
     raise cost
 
 # write a function that uses newtons method to update the joint angles
--- reacher/reacher_manual_control.py
+++ reacher/reacher_manual_control.py
@@ -11,7 +11,8 @@ from sys import platform
 
 flags.DEFINE_bool("run_on_robot", False,
                   "Whether to run on robot or in simulation.")
-flags.DEFINE_bool("ik", False, "Whether to control arms through cartesian coordinates(IK) or joint angles")
+flags.DEFINE_bool(
+    "ik", False, "Whether to control arms through cartesian coordinates(IK) or joint angles")
 FLAGS = flags.FLAGS
 
 KP = 5.0  # Amps/rad
@@ -26,95 +27,100 @@ L2 = 0.11  # meters
 
 
 def main(argv):
-  run_on_robot = FLAGS.run_on_robot
-  reacher = reacher_sim_utils.load_reacher()
-  sphere_id = reacher_sim_utils.create_debug_sphere()
+    run_on_robot = FLAGS.run_on_robot
+    reacher = reacher_sim_utils.load_reacher()
+    sphere_id = reacher_sim_utils.create_debug_sphere()
 
-  joint_ids = reacher_sim_utils.get_joint_ids(reacher)
-  param_ids = reacher_sim_utils.get_param_ids(reacher)
-  reacher_sim_utils.zero_damping(reacher)
+    joint_ids = reacher_sim_utils.get_joint_ids(reacher)
+    param_ids = reacher_sim_utils.get_param_ids(reacher)
+    reacher_sim_utils.zero_damping(reacher)
 
-  p.setPhysicsEngineParameter(numSolverIterations=10)
+    p.setPhysicsEngineParameter(numSolverIterations=10)
 
-  if run_on_robot:
-    serial_port = reacher_robot_utils.get_serial_port()
-    hardware_interface = interface.Interface(serial_port)
-    time.sleep(0.25)
-    hardware_interface.set_joint_space_parameters(kp=KP,
-                                                  kd=KD,
-                                                  max_current=MAX_CURRENT)
-
-  p.setRealTimeSimulation(1)
-  counter = 0
-  last_command = time.time()
-  enable = True
-  joint_angles = np.zeros(6)
-
-  # Use this function to disable/enable certain motors. The first six elements
-  # determine activation of the motors attached to the front of the PCB, which
-  # are not used in this lab. The last six elements correspond to the activations
-  # of the motors attached to the back of the PCB, which you are using.
-  # The 7th element will correspond to the motor with ID=1, 8th element ID=2, etc
-  # hardware_interface.send_dict({"activations": [0, 0, 0, 0, 0, 0, x, x, x, x, x, x]})
-
-  while (1):
     if run_on_robot:
-      hardware_interface.read_incoming_data()
+        serial_port = reacher_robot_utils.get_serial_port()
+        hardware_interface = interface.Interface(serial_port)
+        time.sleep(0.25)
+        hardware_interface.set_joint_space_parameters(kp=KP,
+                                                      kd=KD,
+                                                      max_current=MAX_CURRENT)
 
-    if time.time() - last_command > UPDATE_DT:
-      last_command = time.time()
-      counter += 1
+    p.setRealTimeSimulation(1)
+    counter = 0
+    last_command = time.time()
+    enable = True
+    joint_angles = np.zeros(6)
 
-      slider_angles = np.zeros_like(joint_angles)
-      for i in range(len(param_ids)):
-        c = param_ids[i]
-        targetPos = p.readUserDebugParameter(c)
-        slider_angles[i] = targetPos
+    # Use this function to disable/enable certain motors. The first six elements
+    # determine activation of the motors attached to the front of the PCB, which
+    # are not used in this lab. The last six elements correspond to the activations
+    # of the motors attached to the back of the PCB, which you are using.
+    # The 7th element will correspond to the motor with ID=1, 8th element ID=2, etc
+    # hardware_interface.send_dict({"activations": [0, 0, 0, 0, 0, 0, x, x, x, x, x, x]})
 
-      # If IK is enabled, update joint angles based off of goal XYZ position
-      if FLAGS.ik:
-          xyz = []
-          for i in range(len(param_ids), len(param_ids) + 3):
-            xyz.append(p.readUserDebugParameter(i))
-          xyz = np.asarray(xyz)
-          ret = reacher_kinematics.calculate_inverse_kinematics(xyz, joint_angles[:3])
-          if ret is None:
-            enable = False
-          else:
-            # Wraps angles between -pi, pi
-            joint_angles[:3] = np.arctan2(np.sin(ret), np.cos(ret))
-            joint_angles[3:] = slider_angles[3:]
-            enable = True
-      else:
-        joint_angles = slider_angles
+    while (1):
+        if run_on_robot:
+            hardware_interface.read_incoming_data()
 
-      for i in range(len(joint_ids)):
-        p.setJointMotorControl2(reacher,
-                                joint_ids[i],
-                                p.POSITION_CONTROL,
-                                joint_angles[i],
-                                force=2.)
+        if time.time() - last_command > UPDATE_DT:
+            last_command = time.time()
+            counter += 1
 
-      if run_on_robot and enable:
-        full_actions = np.zeros([3, 4])
-        # TODO: Update order & signs for your own robot/motor configuration like below
-        # left_angles = [-joint_angles[1], -joint_angles[0], joint_angles[2]]
-        # right_angles = [-joint_angles[5], joint_angles[4], -joint_angles[3]]
-        left_angles = joint_angles[:3]
-        right_angles = joint_angles[3:]
-        full_actions[:, 3] = left_angles
-        full_actions[:, 2] = right_angles
+            slider_angles = np.zeros_like(joint_angles)
+            for i in range(len(param_ids)):
+                c = param_ids[i]
+                targetPos = p.readUserDebugParameter(c)
+                slider_angles[i] = targetPos
 
-        hardware_interface.set_actuator_postions(full_actions)
-        # Actuator positions are stored in array: hardware_interface.robot_state.position,
-        # Actuator velocities are stored in array: hardware_interface.robot_state.velocity
+            # If IK is enabled, update joint angles based off of goal XYZ position
+            if FLAGS.ik:
+                xyz = []
+                for i in range(len(param_ids), len(param_ids) + 3):
+                    xyz.append(p.readUserDebugParameter(i))
+                xyz = np.asarray(xyz)
+                print(xyz)
+                ret = reacher_kinematics.calculate_inverse_kinematics(
+                    xyz, joint_angles[:3])
+                if ret is None:
+                    enable = False
+                else:
+                    # Wraps angles between -pi, pi
+                    joint_angles[:3] = np.arctan2(np.sin(ret), np.cos(ret))
+                    joint_angles[3:] = slider_angles[3:]
+                    enable = True
+            else:
+                joint_angles = slider_angles
 
-      end_effector_pos = reacher_kinematics.calculate_forward_kinematics_robot(joint_angles[:3])
-      p.resetBasePositionAndOrientation(sphere_id,
-                                        posObj=end_effector_pos,
-                                        ornObj=[0, 0, 0, 1])
+            for i in range(len(joint_ids)):
+                p.setJointMotorControl2(reacher,
+                                        joint_ids[i],
+                                        p.POSITION_CONTROL,
+                                        joint_angles[i],
+                                        force=2.)
 
-      if counter % 20 == 0:
-        print("Joint angles:", joint_angles, "Position:", end_effector_pos)
+            if run_on_robot and enable:
+                full_actions = np.zeros([3, 4])
+                # TODO: Update order & signs for your own robot/motor configuration like below
+                # left_angles = [-joint_angles[1], -joint_angles[0], joint_angles[2]]
+                # right_angles = [-joint_angles[5], joint_angles[4], -joint_angles[3]]
+                left_angles = joint_angles[:3]
+                right_angles = joint_angles[3:]
+                full_actions[:, 3] = left_angles
+                full_actions[:, 2] = right_angles
+
+                hardware_interface.set_actuator_postions(full_actions)
+                # Actuator positions are stored in array: hardware_interface.robot_state.position,
+                # Actuator velocities are stored in array: hardware_interface.robot_state.velocity
+
+            end_effector_pos = reacher_kinematics.calculate_forward_kinematics_robot(
+                joint_angles[:3])
+            p.resetBasePositionAndOrientation(sphere_id,
+                                              posObj=end_effector_pos,
+                                              ornObj=[0, 0, 0, 1])
+
+            if counter % 20 == 0:
+                print("Joint angles:", joint_angles,
+                      "Position:", end_effector_pos)
+
 
 app.run(main)
